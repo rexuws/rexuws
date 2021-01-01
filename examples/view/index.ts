bf25804d357ab876545ccf2cc3a3f206e479255f
vmtran123 <vmtran.des@gmail.com>
2021-01-01 21:02:21 +0700

    docs: :memo: add examples for view

---
+++ examples/view/index.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import ejs from 'ejs';
+import rex from '../../index';
+
+const app = rex();
+
+app.setView(path.join(__dirname, './views'), {
+  compileMethod: ejs.compile,
+  extName: 'ejs',
+});
+
+app.setView(path.join(__dirname, './views'), {
+  compileMethod: ejs.compile,
+  extName: 'ejs',
+});
+
+app.get('/home', (req, res) => {
+  res.render('Home', {
+    user: {
+      username: 'Adam',
+      email: 'adam@mail.com',
+    },
+    today: new Date(),
+  });
+});
